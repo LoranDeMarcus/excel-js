5482b370286b395691f5f9707747fdc10125fad8
Artyom <akuzikov@mail.ru>
2020-12-23 12:30:08 +0300

    created redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './scss/index.scss';
+import { createStore } from '@core/createStore';
 import { Excel } from '@/components/excel/Excel';
 import { Header } from '@/components/header/Header';
 import { Toolbar } from '@/components/toolbar/Toolbar';
 import { Formula } from '@/components/formula/Formula';
 import { Table } from '@/components/table/Table';
+import { rootReducer } from '@/redux/rootReducer';
+
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
+    components: [Header, Toolbar, Formula, Table],
+    store
 });
-// next lesson: https://vladilen.ru/pl/teach/control/lesson/view?id=161422221
+// next lesson: https://vladilen.ru/pl/teach/control/lesson/view?id=161422300
 excel.render();
