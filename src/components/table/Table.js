a9104a7763392e67651bdb0d86ddc54c2c66e9f1
Artyom <akuzikov@mail.ru>
2020-12-24 09:30:16 +0300

    renamed $dispatch to $emit.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -42,6 +42,7 @@ export class Table extends ExcelComponent {
     selectCell($cell) {
         this.selection.select($cell);
         this.$emit('table:select', $cell);
+        this.$dispatch({ type: 'TEST' });
     }
 
     onMousedown(e) {
